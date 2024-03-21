0765d2c01a72f708bf1a6727f245399bc661be94
KevinAmbler2 <142909819+KevinAmbler2@users.noreply.github.com>
2024-03-21 16:19:06 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 # Import statements
 import tkinter as tk
 from tkinter import ttk
+
+#import PIL
+#from PIL import ImageTk, Image
+
 import random
 
+#--------------------------------------------------------
+# POSSIBLE TO-DO (non-obvious/undocumented features):   |
+#--------------------------------------------------------
+# - Player order tracker and/or turn tracker.           |
+# - Options menu with save/cancel function              |
+#       (documented but highly unfinished).             |
+# - Manual saving/autosave function after               |
+#       player activation (possibly save board          |
+#       arrays/player info data in text files).         |
+# - Savegame loading of ongoing games.                  |
+#--------------------------------------------------------
+# STRETCH GOALS:                                        |
+#--------------------------------------------------------
+# - Expansion pack content (5-6 player variant, cities  |
+#       and knights, etc).                              |
+#--------------------------------------------------------
+# PERSONAL TO-DO (QoL stuff or "dev infrastructure"):   |
+#--------------------------------------------------------
+# - Change tile relationship doc to be self-updating.   |
+#--------------------------------------------------------
 
-numPlayers = 2
+
+# UNIVERSAL CONSTANTS
+MIN_PLAYERS = 2
+MAX_PLAYERS = 4
 
 
-# class BoardGeneration():
-#     def __init__(self, parent):
-#         super().__init__(parent)
+class BoardGeneration():
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        global boardTileArray
+        boardTileArray = []
 
 
 class Window(tk.Toplevel):
@@ -24,75 +54,116 @@ class Window(tk.Toplevel):
 
         if type == "menu":
             self.geometry("300x200")
-            ttk.Button(self,
+            tk.Button(self,
                        text="Play Game",
                        command=lambda: [root.open_window("Game Setup", "setup"), self.withdraw()]).pack(side=tk.TOP, pady=10)
-            ttk.Button(self,
+            tk.Button(self,
                        text="Options",
                        command=lambda: [root.open_window("Options", "options"), self.withdraw()]).pack()
-            ttk.Button(self,
+            tk.Button(self,
                        text="Exit",
                        command=root.close_confirm).pack(side=tk.BOTTOM)
 
         elif type == "options":  # UNFINISHED
-            ttk.Button(self,
+            tk.Button(self,
                        text="Save options",
                        command=lambda: [menu.deiconify(), self.destroy()]).pack(side=tk.LEFT)  # UNFINISHED
-            ttk.Button(self,
+            tk.Button(self,
                        text="Cancel",
                        command=lambda: [menu.deiconify(), self.destroy()]).pack(side=tk.RIGHT)
 
         elif type == "setup":
             numPlayersStr = tk.StringVar()
             numPlayersEntry = tk.Entry(self, textvariable=numPlayersStr)
-            numPlayersEntry.insert(0, "Enter the number of players, 2-6")
+            numPlayersEntry.insert(0, "Enter the number of players, "+str(MIN_PLAYERS)+"-"+str(MAX_PLAYERS))
             numPlayersEntry.pack(expand=True, side=tk.TOP)
 
             def addPlayers():
                 global numPlayers
                 numPlayers = int(numPlayersEntry.get())
-                TEMPcreatePlayerWindows(numPlayers)
-                #print(numPlayers)
+                if numPlayers <= MAX_PLAYERS and numPlayers >= MIN_PLAYERS:
+                    root.open_window("Board", "board")
+                    TEMPcreatePlayerWindows(numPlayers)
+                    self.destroy()
+                    #print(numPlayers)
+                else:
+                    root.open_window("Error", "playerCountError")
+            
+            def playerColorGen(window, color):
+                window.configure(background=color)
+                tk.Label(window,
+                        text="Player Number "+str(currPlayerNum),
+                        background=color).pack()
+                # HIDES ALL OTHER PLAYERS + BOARD IF NO OTHER ACTION TAKEN FIRST -> Locks up if no other windows available/open
+                tk.Button(window,
+                        text="Close this window",
+                        command=window.destroy).pack(side=tk.RIGHT)
             
             def TEMPcreatePlayerWindows(numPlayers):
-                global playerNum
-                playerNum = 1
-                while playerNum <= numPlayers:
-                    root.open_window("Player "+str(playerNum), "playerSheet")
-                    playerNum += 1
+                global currPlayerNum
+                currPlayerNum = 1
+                while currPlayerNum <= numPlayers:
+                    newPlayerSheet = root.open_window("Player "+str(currPlayerNum), "playerSheet")
+                    newPlayerSheet.geometry("200x400")
 
-            ttk.Button(self,
+                    if currPlayerNum == 1:
+                        playerColorGen(newPlayerSheet, "red")
+                    elif currPlayerNum == 2:
+                        playerColorGen(newPlayerSheet, "blue")
+                    elif currPlayerNum == 3:
+                        playerColorGen(newPlayerSheet, "orange")
+                    elif currPlayerNum == 4:
+                        playerColorGen(newPlayerSheet, "yellow")
+
+                    # Extra player setup for if expansions are included
+                    elif currPlayerNum == 5:
+                        playerColorGen(newPlayerSheet, "green")
+                    else:
+                        playerColorGen(newPlayerSheet, "white")
+
+                    currPlayerNum += 1
+
+            tk.Button(self,
                        text="Continue",
-                       command=lambda: [root.open_window("Board", "board"), addPlayers(), self.destroy()]).pack(side=tk.LEFT)  # UNFINISHED
-            ttk.Button(self,
+                       command=lambda: addPlayers()).pack(side=tk.LEFT)  # UNFINISHED
+            tk.Button(self,
                        text="Back",
-                       command=lambda: [menu.deiconify(), self.destroy()]).pack(side=tk.RIGHT)
+                       command=lambda: [menu.deiconify(), self.destroy()]).pack(side=tk.RIGHT) # Hide menu and destroy setup page
 
         elif type == "board":
             self.geometry("600x600")
-            ttk.Button(self,
+            tk.Button(self,
                        text="Open new window",
                        command=lambda: root.open_window("Board", "board")).pack(side=tk.LEFT)
             # HIDES ALL PLAYERS IF NO OTHER ACTION TAKEN FIRST -> Locks up if no other windows available/open
-            ttk.Button(self,
+            tk.Button(self,
                        text="Close this window",
                        command=self.destroy).pack(side=tk.RIGHT)
         
-        elif type == "playerSheet":
+            '''elif type == "playerSheet":
             self.geometry("200x400")
-            ttk.Label(self, text="Player Number "+str(playerNum))
+            ttk.Label(self,text="Player Number "+str(playerNum)).pack()
             # HIDES ALL OTHER PLAYERS + BOARD IF NO OTHER ACTION TAKEN FIRST -> Locks up if no other windows available/open
             ttk.Button(self,
                        text="Close this window",
-                       command=self.destroy).pack(side=tk.RIGHT)
+                       command=self.destroy).pack(side=tk.RIGHT)'''
+        
+        elif type == "playerCountError": # Make a subclass of an error type?
+            self.geometry("250x100")
+            self.resizable(False,False)
+            tk.Label(self,text=("Please enter a number of players between "+str(MIN_PLAYERS)+"-"+str(MAX_PLAYERS)+".")).pack()
+            tk.Button(self,
+                       text="OK",
+                       command=self.destroy).pack(side=tk.BOTTOM)
 
         elif type == "exit":
             # If close is confirmed, calls close_all function
-            ttk.Button(self,
+            tk.Label(self,text="Are you sure you want to close?").pack()
+            tk.Button(self,
                        text="Close all windows",
                        command=root.close_all).pack(side=tk.LEFT)
             # If close is not confirmed, closes confirmation window
-            ttk.Button(self,
+            tk.Button(self,
                        text="Back",
                        command=self.destroy).pack(side=tk.RIGHT)
 
@@ -109,7 +180,7 @@ class Root(tk.Tk):
 
     def open_window(self, title, type):
         window = Window(self, title, type)
-        if type == "exit" or type == "options":  # If exit confirmation window, forces user interaction
+        if type == "exit" or type == "options" or type == "playerCountError":  # If exit confirmation window, forces user interaction
             window.grab_set()
 
         return window
